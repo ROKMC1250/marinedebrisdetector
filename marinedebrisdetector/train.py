7f81143b53d54abaa8f568e34f7de4402735f6ff
ROKMC1250 <ROMKMC1250@github.com>
2024-05-14 13:32:59 +0900

    gpu_allocate

--- marinedebrisdetector/train.py
+++ marinedebrisdetector/train.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 import os
 import argparse
-
+import torch
 from data.plastic_litter_project import PLPDataset
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
@@ -105,4 +105,15 @@ def main(args):
 
 
 if __name__ == '__main__':
+    GPU_NUM = 3 # 원하는 GPU 번호 입력
+    device = torch.device(f'cuda:{GPU_NUM}' if torch.cuda.is_available() else 'cpu')
+    torch.cuda.set_device(device) # change allocation of current GPU
+    print ('Current cuda device ', torch.cuda.current_device()) # check
+
+    # Additional Infos
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(GPU_NUM))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(GPU_NUM)/1024**3,1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_cached(GPU_NUM)/1024**3,1), 'GB')
     main(parse_args())
